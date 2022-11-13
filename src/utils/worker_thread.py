97edd79a33bbbd6e1a1f125e92964e504b20bae3
MorroBludvist <igorek.sekirin@gmail.com>
2022-11-13 15:50:34 +0300

    fix: add windows compatibility and new bug

--- src/utils/worker_thread.py
+++ src/utils/worker_thread.py
@@ -1,17 +1,31 @@
 from time import sleep
 
 from PyQt5.QtCore import QThread, pyqtSignal
-from serial import Serial
+from serial import Serial, SerialException
 
 
 class WorkerThread(QThread):
     messageReceived = pyqtSignal(str)
 
     def __init__(self, port: str, parent=None):
+        self.port = port
         QThread.__init__(self, parent)
-        self.serialDevice = Serial(f"/dev/{port}")
+        try:
+            self.serialDevice = Serial(f"/dev/{self.port}")
+        except SerialException:
+            self.serialDevice = Serial(f"{self.port}")
         self.exiting = False
         self.start()
+
+    def check_port(self):
+        try:
+            try:
+                self.serialDevice = Serial(f"/dev/{self.port}")
+            except SerialException:
+                self.serialDevice = Serial(f"{self.port}")
+        except SerialException:
+            return False
+        return True
 
     def run(self):
         while not self.exiting:
@@ -25,3 +39,4 @@ class WorkerThread(QThread):
     def __del__(self):
         self.exiting = True
         self.wait()
+
